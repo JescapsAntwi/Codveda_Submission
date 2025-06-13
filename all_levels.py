8f192ba2881cab105596cf42afb3d0cdbbb4fe58
JescapsAntwi <antwijescaps1@gmail.com>
2025-06-13 15:47:17 +0000

    Modified all_levels.py

--- all_levels.py
+++ all_levels.py
@@ -153,7 +153,7 @@ y_pred_house = lr_model.predict(X_test_house)
 # Evaluate the model based on things it hasn't seen before (the testing set it never saw during training)
 print("\n3. Model Evaluation:")
 r2 = r2_score(y_test_house, y_pred_house) #between 0 and 1
-mse = mean_squared_error(y_test_house, y_pred_house)
+mse = mean_squared_error(y_test_house, y_pred_house) #this number should be as low as possible
 rmse = np.sqrt(mse)
 
 print(f"   - R-squared Score: {r2:.4f}")
